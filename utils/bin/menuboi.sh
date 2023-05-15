bee0483cbb270acc623e4a351839a06d0b88f5da
must_eat <mss@tutanota.de>
2023-05-15 12:22:22 +0300

    upd: (utils/) > bin/:  - maimcrop; rermove local keyword, not called from a funch.  - menuboi.sh -> dwmenu:   * remove apathy ainit specific reboot and poweroff commands.   * add chromium and xkill. > etc/sourceme:  - /mss/bin/ is appended from the handler now.  - use append_path for others.
    add: (utils/)
    > bin/chromium; move the script from the apathy build recipe for chromium.
    > share/:
     - bash-handler.
     - nfqws-handler.

--- utils/bin/menuboi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-killdwm(){ kill -TERM -"$(cat /proc/`pidof dwm`/stat | awk '{print $5}')" ;}
-
-case "$(printf "kill-dwm\nreboot\npoweroff" | dmenu -b -p "take ya pick:")" in
- poweroff) killdwm && doas -- kill -USR1 1 ;;
- reboot)   killdwm && doas -- kill -INT  1 ;;
- kill-dwm) killdwm                         ;;
-esac
