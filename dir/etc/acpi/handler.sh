d29d337ddbc5648c38d0b7ee5d9e9cd17b02a7f5
must_eat <mss@tutanota.de>
2023-05-15 20:19:52 +0300

    rwr: (dir/) etc/acpi/handler.sh > source /etc/profile and /mss/etc/funcs. > add xorgexec() and userexec(). > remove dud prompts. > button/vendor -> button/prog1. > call xtrlock when suspending to ram. > call setbright +/- for video/brightness{up,down}. > setbright to 10 when unplugged from ac. > call pactl with userexec(). > handle dud for button/{up,down,left,right,kpenter} to avoid journal spam.
    rmv: (dir/) etc/resolv.conf; unnecessary.

--- dir/etc/acpi/handler.sh
+++ dir/etc/acpi/handler.sh
@@ -1,73 +1,54 @@
 #!/bin/sh
+. /etc/profile
+. /mss/etc/funcs
 psink="@DEFAULT_SINK@"
  psrc="@DEFAULT_SOURCE@"
 
+xorgexec(){
+ if pidof Xorg >/dev/null 2>&1
+  then
+   DISPLAY=:0 ${@}
+  else
+   aprint_fail "X is not running."
+   exit 1
+ fi
+}
+
+userexec(){
+ sudo -u mss -- ${@}
+}
+
 case "${1}" in
  # keyboard
- button/fnf1)          : ;;
- button/fnf11)         : ;;
- button/fnf9)          : ;;
- button/suspend)       : ;;
- button/wlan)          : ;;
- button/zoom)          : ;;
- video/brightnessdown) : ;;
- video/brightnessup)   : ;;
-
- button/screenlock)    xorgexec xtrlock                         ;;
+ button/screenlock)    xorgexec xtrlock                                 ;;
  button/sleep)         xorgexec xtrlock &
-                       [ "${2}" = "SBTN" ] && suspendtodisk yes ;;
- video/switchmode)     xorgexec setmon                          ;;
- cd/stop)              mpc -q stop                              ;;
- cd/prev)              mpc -q prev                              ;;
- cd/play)              mpc -q toggle                            ;;
- cd/next)              mpc -q next                              ;;
+                       [ "${2}" = "SBTN" ] && suspendtodisk yes         ;;
+ button/prog1)         xorgexec xtrlock &
+                       printf '%s' 'mem' > /sys/power/state             ;;
 
- # extra keys
- button/mute)          pactl set-sink-mute   "${psink}" toggle  ;;
- button/volumedown)    pactl set-sink-volume "${psink}" -5%     ;;
- button/volumeup)      pactl set-sink-volume "${psink}" +5%     ;;
- button/micmute)       pactl set-source-mute "${psrc}"  toggle  ;;
- button/vendor)        printf '%s' 'mem' > /sys/power/state     ;;
+ video/switchmode)     xorgexec setmon                                  ;;
 
- # lid
- button/lid)
-  case "${3}" in
-   open)               : ;;
-   close)              : ;;
-  esac
- ;;
+ cd/stop)              mpc -q stop                                      ;;
+ cd/prev)              mpc -q prev                                      ;;
+ cd/play)              mpc -q toggle                                    ;;
+ cd/next)              mpc -q next                                      ;;
+ button/mute)          userexec pactl set-sink-mute   "${psink}" toggle ;;
+ button/micmute)       userexec pactl set-source-mute "${psrc}"  toggle ;;
+ button/volumedown)    userexec pactl set-sink-volume "${psink}" -5%    ;;
+ button/volumeup)      userexec pactl set-sink-volume "${psink}" +5%    ;;
 
- # charger
+ video/brightnessdown) setbright -                                      ;;
+ video/brightnessup)   setbright +                                      ;;
+
  ac_adapter)
   case "${4}" in
-   00000000)           setbright 5   && setgov powersave        ;;
-   00000001)           setbright max && setgov schedutil        ;;
+   00000000)           setbright 10  && setgov powersave                ;;
+   00000001)           setbright max && setgov schedutil                ;;
   esac
  ;;
 
- # battery
- battery)
-  case "${4}" in
-   00000000)           : ;;
-   00000001)           : ;;
-  esac
- ;;
+ button/up|button/down|button/left|button/right|button/kpenter)         ;;
 
- # headphone in
- jack/headphone)
-  case "${3}" in
-   plug)               : ;;
-   unplug)             : ;;
- esac
- ;;
-
- # mic in
- jack/microphone)
-  case "${3}" in
-   plug)               : ;;
-   unplug)             : ;;
- esac
- ;;
  *)
   printf "acpi.script: event not understood:\n${1}\n" > /dev/kmsg
  ;;
