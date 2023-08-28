4e3451b68ee4a49430fc68e1478fe38bb9407d3a
must_eat <mss@tutanota.de>
2023-08-28 08:30:37 +0300

    upd: (utils/) bin/, setfirewall.py: > iface -> WAN_IFACE. > handle_eth_* -> handle_wan_*. > nftrun():  - add capture_output to subprocess.run args.  - if not quiet, return the exit code and stdout.  - if we catch subprocess.CalledProcessError, print the stderr. > genports(); better exception message. > when generating the WAN_IFACE, check for flags as well:  - 0001 -> iface is up  - 0002 -> iface is gateway  - 0003 -> iface is up and is gateway > in the mynat table create the postrouting table as well. > use nftrun() to gen the ruleset, and write at once, instead of calling Popen.

--- utils/bin/setfirewall.py
+++ utils/bin/setfirewall.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # pylint: disable=missing-module-docstring,missing-function-docstring
+import argparse
 import os
 import shutil
 import subprocess
-import argparse
 
 # 1 > parse args
 PARSER_DESC = "Create a baseline firewall using nftables with rules that do "
-PARSER_DESC += "not collide with the non-internet-facing interfaces."
-PARSER_TCP_DESC = "List of TCP ports allowed on the internet-facing interface."
-PARSER_UDP_DESC = "List of UDP ports allowed on the internet-facing interface."
+PARSER_DESC += "not collide with the non-WAN interfaces."
+PARSER_TCP_DESC = "List of TCP ports allowed on the WAN interface."
+PARSER_UDP_DESC = "List of UDP ports allowed on the WAN interface."
 PARSER_DESK_DESC = "Install the NAT rules for allowing rootless HTTP(s) servers."
 
 parser = argparse.ArgumentParser(description=PARSER_DESC)
@@ -22,21 +22,26 @@ tcp_ports = args.tcp
 udp_ports = args.udp
 desktop = args.desktop
 
-# pylint: disable=invalid-name
-iface = None
+WAN_IFACE = None
 
 
 # 2 > functions
-def nftrun(string):
+# pylint: disable=inconsistent-return-statements
+def nftrun(string, quiet=True):
     cmdline = string.split(" ")
     cmdline.insert(0, "nft")
-    subprocess.run(cmdline, check=True)
+    try:
+        proc = subprocess.run(cmdline, check=True, capture_output=True)
+    except subprocess.CalledProcessError as except_obj:
+        raise ValueError(except_obj.stderr.decode("utf-8")) from except_obj
+    if not quiet:
+        return (proc.returncode, proc.stdout.decode("utf-8"))
 
 
 def genports(ports):
     for port in ports:
         if port <= 0 or port > 65535:
-            raise ValueError(f"TCP port {port} is invalid.")
+            raise ValueError(f"{port} is not a valid port number.")
 
     ports, ports_str = list(set(ports)), ""
 
@@ -55,15 +60,15 @@ if os.getuid() != 0:
 if not shutil.which("nft"):
     raise ValueError("`nft' is not found in path.")
 
-with open("/proc/net/route", "r", encoding="utf-8") as f:
-    for line in f.readlines():
-        line = line.strip().split()
-        if line[1] == "00000000":
-            iface = line[0]
-            break
+with open("/proc/net/route", "r", encoding="utf-8") as routes:
+    routes = [i for i in routes.read().split("\n") if i != ""][1:]
+    for iface in routes:
+        iface = iface.strip().split()
+        if iface[1] == "00000000" and iface[3] in ["0003", "0002"]:
+            WAN_IFACE = iface[0]
 
-if iface is None:
-    raise ValueError("No interface with a default route.")
+if not WAN_IFACE:
+    raise ValueError("E: No interface with a default route found.")
 
 if tcp_ports is None and udp_ports is None:
     raise ValueError("You have not allowed any TCP or UDP ports.")
@@ -72,7 +77,8 @@ tcp_ports = genports(tcp_ports)
 udp_ports = genports(udp_ports)
 
 # 4 > action
-print(f" > Default interface is: {iface}")
+print(f" > Default interface is: {WAN_IFACE}")
+
 print("  - Flushing ruleset.")
 nftrun("flush ruleset")
 
@@ -95,59 +101,61 @@ for chain in ["input", "output", "forward"]:
     runstr += " priority -200; policy accept;}"
     nftrun(runstr)
 
-for chain in ["prerouting", "output"]:
+for chain in ["prerouting", "postrouting", "output"]:
     runstr = f"create chain ip mynat {chain} {{type nat hook {chain}"
     runstr += " priority mangle; policy accept;}"
     nftrun(runstr)
 
-print("  - Creating handlers for the internet-facing interfaces.")
-nftrun("create chain ip myfilter handle_eth_filter_input")
-nftrun("create chain ip mynat handle_eth_nat_prerouting")
-nftrun("create chain ip mynat handle_eth_nat_output")
+print("  - Creating handlers for the WAN interface.")
+nftrun("create chain ip myfilter handle_wan_filter_input")
+nftrun("create chain ip mynat handle_wan_nat_prerouting")
+nftrun("create chain ip mynat handle_wan_nat_output")
 
 print("  - Creating base filter<-input rules.")
 nftrun("add rule ip myfilter input ct state established,related accept")
 nftrun('add rule ip myfilter input iifname "lo" accept')
 nftrun('add rule ip myfilter input iifname != "lo" ip daddr 127.0.0.0/8 drop')
 nftrun("add rule ip myfilter input icmp type echo-request accept")
-nftrun(f"add rule ip myfilter input iifname {iface} jump handle_eth_filter_input")
+nftrun(f"add rule ip myfilter input iifname {WAN_IFACE} jump handle_wan_filter_input")
 
-print("  - Creating the internet-facing interface handler rules for filter<-input.")
-nftrun("add rule ip myfilter handle_eth_filter_input tcp dport @tcp_allow accept")
-nftrun("add rule ip myfilter handle_eth_filter_input udp dport @udp_allow accept")
-nftrun("add rule ip myfilter handle_eth_filter_input counter drop")
+print("  - Creating the WAN interface handler rules for filter<-input.")
+nftrun("add rule ip myfilter handle_wan_filter_input tcp dport @tcp_allow accept")
+nftrun("add rule ip myfilter handle_wan_filter_input udp dport @udp_allow accept")
+nftrun("add rule ip myfilter handle_wan_filter_input counter drop")
 
 print("  - Creating base nat<-prerouting rules.")
 nftrun("add rule ip mynat prerouting ct state established,related accept")
-nftrun(f"add rule ip mynat prerouting iifname {iface} jump handle_eth_nat_prerouting")
+nftrun(
+    f"add rule ip mynat prerouting iifname {WAN_IFACE} jump handle_wan_nat_prerouting"
+)
 
 if desktop:
-    print(
-        "  - Creating the internet-facing interface handler rules for nat<-prerouting."
-    )
-    nftrun("add rule ip mynat handle_eth_nat_prerouting tcp dport 80 redirect to :1337")
+    print("  - Creating the WAN interface handler rules for nat<-prerouting.")
+    nftrun("add rule ip mynat handle_wan_nat_prerouting tcp dport 80 redirect to :1337")
     nftrun(
-        "add rule ip mynat handle_eth_nat_prerouting tcp dport 443 redirect to :1338"
+        "add rule ip mynat handle_wan_nat_prerouting tcp dport 443 redirect to :1338"
     )
 
-nftrun("add rule ip mynat handle_eth_nat_prerouting tcp dport @tcp_allow accept")
-nftrun("add rule ip mynat handle_eth_nat_prerouting udp dport @udp_allow accept")
-nftrun("add rule ip mynat handle_eth_nat_prerouting counter drop")
+nftrun("add rule ip mynat handle_wan_nat_prerouting tcp dport @tcp_allow accept")
+nftrun("add rule ip mynat handle_wan_nat_prerouting udp dport @udp_allow accept")
+nftrun("add rule ip mynat handle_wan_nat_prerouting counter drop")
 
 if desktop:
     print("  - Creating base nat<-output rules.")
-    nftrun(f"add rule ip mynat output oifname {iface} jump handle_eth_nat_output")
-    print("  - Creating the internet-facing interface handler rules for nat<-output.")
+    nftrun(f"add rule ip mynat output oifname {WAN_IFACE} jump handle_wan_nat_output")
+    print("  - Creating the WAN interface handler rules for nat<-output.")
     nftrun(
-        "add rule ip mynat handle_eth_nat_output ip daddr 127.0.0.1 tcp dport 80 redirect to :1337"
+        "add rule ip mynat handle_wan_nat_output ip daddr 127.0.0.1 tcp dport 80 redirect to :1337"
     )
     nftrun(
-        "add rule ip mynat handle_eth_nat_output ip daddr 127.0.0.1 tcp dport 443 redirect to :1338"
+        "add rule ip mynat handle_wan_nat_output ip daddr 127.0.0.1 tcp dport 443 redirect to :1338"
     )
 
 # 5 > write to disk
 print(" > Writing changes to disk.")
+
+NFTABLES_CONF = "flush ruleset\n\n"
+NFTABLES_CONF += nftrun("-s list ruleset", quiet=False)[1]
+
 with open("/etc/nftables.conf", "w", encoding="utf-8") as file:
-    file.write("flush ruleset\n\n")
-    # pylint: disable=consider-using-with
-    subprocess.Popen(["nft", "-s", "list", "ruleset"], stdout=file)
+    file.write(NFTABLES_CONF)
