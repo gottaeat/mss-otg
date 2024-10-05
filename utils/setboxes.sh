831aff473aa734d0a9ac1df3a2561d1648bbf70a
must_eat <mss@tutanota.de>
2024-10-05 20:20:11 +0300

    upd: (utils/) setboxes.sh -> setboxes.sh.in; template paths.

--- utils/setboxes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ ! -L "${DESTDIR}/mss/work" ]; then
-    ln -sf /mnt/mss/stuff/techy-bits/work "${DESTDIR}/mss/work"
-fi
-
-if [ ! -L "${DESTDIR}/mss/repo" ]; then
-    ln -sf /mnt/mss/stuff/techy-bits/git/setboxes "${DESTDIR}/mss/repo"
-fi
