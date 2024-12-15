5675c6307b4933d9f8b8e9c53ffb2fbd66614737
must_eat <eateing@loggedin.net>
2024-12-15 22:31:37 +0300

    rwr: (utils/) bin/statusbar.py: > add get_url() from gottaeat/bands, make it accept headers. > modularize everything, remove argparse, use sys.argv. > make scrapers utilize get_url() instead of their own try-except. > leave MPD() as a class, add module_mpd() as its instance.

--- utils/bin/statusbar.py
+++ utils/bin/statusbar.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
-# pylint: disable=invalid-name
-# pylint: disable=too-few-public-methods
 # pylint: disable=bare-except
-import argparse
+# pylint: disable=inconsistent-return-statements
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+
 import json
 import os
 import platform
@@ -15,221 +16,245 @@ import time
 import urllib.parse
 import urllib.request
 
-from threading import Thread
+
+# - - utils - - #
+def get_url(
+    url, headers={}, tls_context=None
+):  # pylint: disable=dangerous-default-value
+    # set useragent
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    headers["User-Agent"] += "AppleWebKit/537.36 (KHTML, like Gecko) "
+    headers["User-Agent"] += "Chrome/126.0.0.0 Safari/537.36"
+
+    request = urllib.request.Request(
+        url,
+        headers=headers,
+    )
+
+    # get response
+    if tls_context:
+        response = urllib.request.urlopen(
+            request, context=tls_context
+        )  # pylint: disable=consider-using-with
+    else:
+        response = urllib.request.urlopen(  # pylint: disable=consider-using-with
+            request
+        )
+
+    # - - content-type - - #
+    content_type = response.getheader("Content-Type")
+
+    if not content_type:
+        response.close()
+        raise ValueError("Content-Type header was not found")
+
+    content_type = content_type.split(";")
+
+    # check if we take the mimetype
+    mimetype = content_type[0]
+
+    if mimetype not in (
+        "application/javascript",
+        "application/json",
+        "application/xml",
+        "text/html",
+        "text/javascript",
+    ):
+        response.close()
+        raise ValueError(f'"{mimetype}" is not an accepted mimetype')
+
+    # check if charset was specified
+    try:
+        charset = content_type[1].split("charset=")[1]
+    except IndexError:
+        charset = "utf-8"
+
+    # - - content-length - - #
+    content_length = response.getheader("Content-Length")
+    max_size = 1024 * 1024 * 5
+
+    if content_length:
+        if int(content_length) > max_size:
+            response.close()
+            content_size = content_length / 1024 / 1024
+            raise ValueError(f"file is larger than 5MB ({content_size})")
+
+    # read data
+    data = response.read(max_size + 1)
+    response.close()
+
+    if len(data) > max_size:
+        raise ValueError("file is larger than 5MB")
+
+    if len(data) == 0:
+        raise ValueError("empty data returned")
+
+    # decode data
+    try:
+        data = data.decode(encoding=charset)
+    except:
+        try:
+            data = data.decode(encoding="utf-8")
+        except UnicodeDecodeError:
+            data = data.decode(encoding="latin-1")
+
+    return data
 
 
-# mpd
-# class MPD:
-#    def __init__(self, mpd_host, mpd_port):
-#        self._address = (socket.gethostbyname(mpd_host), mpd_port)
-#
-#    def _check_status(self):
-#        try:
-#            check = socket.socket()
-#            check.connect(self._address)
-#        except ConnectionRefusedError:
-#            return (False, "dead")
-#
-#        mpd_check = check.recv(1024).decode()
-#        if "OK MPD" in mpd_check:
-#            status = (True, "alive")
-#        else:
-#            status = (False, "not MPD")
-#
-#        check.close()
-#        return status
-#
-#    def _query(self, query):
-#        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#        client.connect(self._address)
-#        client.send(query.encode())
-#
-#        sockfile = client.makefile(mode="r")
-#
-#        raw = ""
-#        while True:
-#            line = sockfile.readline()
-#
-#            if "OK MPD " in line:
-#                continue
-#            if line == "\r\n":
-#                continue
-#            if "OK\n" in line:
-#                break
-#
-#            raw += line
-#
-#        client.close()
-#
-#        querylist = raw.split("\n")
-#        querylist.remove("")
-#
-#        return querylist
-#
-#    @staticmethod
-#    def _parse(response):
-#        final = {}
-#        for line in response:
-#            data = line.split(":", maxsplit=1)
-#            final[data[0]] = data[1].lstrip()
-#
-#        return final
-#
-#    def run(self):
-#        daemon_status = self._check_status()
-#
-#        if not daemon_status[0]:
-#            return daemon_status[1]
-#
-#        status = self._parse(self._query("status\n"))
-#
-#        if status["state"] == "stop":
-#            return "idle"
-#
-#        cursong = self._parse(self._query("currentsong\n"))
-#
-#        if len(cursong) == 0:
-#            return "idle"
-#
-#        songname = cursong.get("Title", re.sub(r"^.*\/|\.[^.]*$", "", cursong["file"]))
-#
-#        if len(songname) >= 20:
-#            songname = songname[0:17] + "..."
-#
-#        if status["state"] == "pause":
-#            indic = "[#]"
-#        else:
-#            cur_dur = float(cursong["duration"]) - float(status["elapsed"])
-#
-#            if cur_dur >= 3600:
-#                timeshit = "%H:%M:%S"
-#            else:
-#                timeshit = "%M:%S"
-#
-#            indic = f"[{time.strftime(timeshit, time.gmtime(cur_dur))}]"
-#
-#        return f"{indic} {songname}"
+# - - mpd client - - #
+class MPD:  # pylint: disable=too-few-public-methods
+    def __init__(self, mpd_host, mpd_port):
+        self._address = (socket.gethostbyname(mpd_host), mpd_port)
 
+    def _check_status(self):
+        try:
+            check = socket.socket()
+            check.connect(self._address)
+        except ConnectionRefusedError:
+            return (False, "dead")
 
-# scrape
-class Scrape:
-    def __init__(self, area):
-        self.area = area
-        self.mgm = "cookin"
-        self.yahoo = "cookin"
-        self.binance = "cookin"
+        mpd_check = check.recv(1024).decode()
+        if "OK MPD" in mpd_check:
+            status = (True, "alive")
+        else:
+            status = (False, "not MPD")
 
-    def get_mgm(self):
-        context = ssl.create_default_context()
-        context.options |= 0x4
+        check.close()
+        return status
 
-        url = "https://servis.mgm.gov.tr/web/sondurumlar?"
-        params = urllib.parse.urlencode({"merkezid": self.area})
+    def _query(self, query):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(self._address)
+        client.send(query.encode())
 
-        req = url + params
-        headers = {"Origin": "https://mgm.gov.tr"}
+        sockfile = client.makefile(mode="r")
 
-        req = urllib.request.Request(req, headers=headers)
-
+        raw = ""
         while True:
-            try:
-                with urllib.request.urlopen(req, context=context) as f:
-                    data = f.read().decode()
-            except urllib.request.HTTPError as e:
-                self.mgm = f"HTTP {e.code}"
-                time.sleep(err_sleep)
-            except urllib.error.URLError as e:
-                self.mgm = f"URL {e.reason.errno}"
-                time.sleep(err_sleep)
-            except:
-                self.mgm = "Error"
-                time.sleep(err_sleep)
+            line = sockfile.readline()
+
+            if "OK MPD " in line:
+                continue
+            if line == "\r\n":
+                continue
+            if "OK\n" in line:
+                break
+
+            raw += line
+
+        client.close()
+
+        querylist = raw.split("\n")
+        querylist.remove("")
+
+        return querylist
+
+    @staticmethod
+    def _parse(response):
+        final = {}
+        for line in response:
+            data = line.split(":", maxsplit=1)
+            final[data[0]] = data[1].lstrip()
+
+        return final
+
+    def run(self):
+        daemon_status = self._check_status()
+
+        if not daemon_status[0]:
+            return daemon_status[1]
+
+        status = self._parse(self._query("status\n"))
+
+        if status["state"] == "stop":
+            return "idle"
+
+        cursong = self._parse(self._query("currentsong\n"))
+
+        if len(cursong) == 0:
+            return "idle"
+
+        songname = cursong.get("Title", re.sub(r"^.*\/|\.[^.]*$", "", cursong["file"]))
+
+        if len(songname) >= 20:
+            songname = songname[0:17] + "..."
+
+        if status["state"] == "pause":
+            indic = "[#]"
+        else:
+            cur_dur = float(cursong["duration"]) - float(status["elapsed"])
+
+            if cur_dur >= 3600:
+                timeshit = "%H:%M:%S"
             else:
-                try:
-                    data = json.loads(data)[0]
-                    code = data["hadiseKodu"]
-                    temp = data["sicaklik"]
-                    wind = data["ruzgarHiz"]
-                    rain = data["yagis00Now"]
+                timeshit = "%M:%S"
 
-                    temp = f"{temp}°" if str(temp) != "-9999" else "n/a"
-                    code = code.lower() if str(code) != "-9999" else "n/a"
-                    wind = f"{wind:.1f}km/h" if str(wind) != "-9999" else "n/a"
-                    rain = f"{rain}mm" if str(rain) != "-9999" else "n/a"
+            indic = f"[{time.strftime(timeshit, time.gmtime(cur_dur))}]"
 
-                    self.mgm = f"{temp} {code} {wind} {rain}"
-                except:
-                    self.mgm = "Parse Error"
-                    time.sleep(err_sleep)
-                else:
-                    time.sleep(interval)
-
-    def get_yahoo(self):
-        url = "https://query1.finance.yahoo.com/v8/finance/chart/"
-        params = urllib.parse.urlencode({"USDTRY": "X"})
-        req = url + params
-
-        while True:
-            try:
-                with urllib.request.urlopen(req) as f:
-                    data = f.read().decode()
-            except urllib.request.HTTPError as e:
-                self.yahoo = f"HTTP {e.code}"
-                time.sleep(err_sleep)
-            except urllib.error.URLError as e:
-                self.yahoo = f"URL {e.reason.errno}"
-                time.sleep(err_sleep)
-            except:
-                self.yahoo = "Error"
-                time.sleep(err_sleep)
-            else:
-                try:
-                    self.yahoo = json.loads(data)["chart"]["result"][0]["meta"][
-                        "regularMarketPrice"
-                    ]
-                except:
-                    self.yahoo = "Parse Error"
-                    time.sleep(err_sleep)
-                else:
-                    time.sleep(interval)
-
-    def get_binance(self):
-        url = "https://api.binance.com/api/v3/ticker/price?"
-        params = urllib.parse.urlencode({"symbol": "USDTTRY"})
-        req = url + params
-
-        while True:
-            try:
-                with urllib.request.urlopen(req) as f:
-                    data = f.read().decode()
-            except urllib.request.HTTPError as e:
-                self.binance = f"HTTP {e.code}"
-                time.sleep(err_sleep)
-            except urllib.error.URLError as e:
-                self.binance = f"URL {e.reason.errno}"
-                time.sleep(err_sleep)
-            except:
-                self.binance = "Error"
-                time.sleep(err_sleep)
-            else:
-                try:
-                    self.binance = float(json.loads(data)["price"].rstrip("0"))
-                except:
-                    self.binance = "Parse Error"
-                    time.sleep(err_sleep)
-                else:
-                    time.sleep(interval)
+        return f"{indic} {songname}"
 
 
-# procfs
-IBMFAN_PATH = "/proc/acpi/ibm/fan"
+def module_mpd():
+    return MPD("localhost", 6600).run()
 
 
-def get_loadavg():
-    return f"{os.getloadavg()[0]:.2f}"
+# - - scrape modules - - #
+def module_mgm(area):
+    context = ssl.create_default_context()
+    context.options |= 0x4
+
+    try:
+        data = get_url(
+            f"https://servis.mgm.gov.tr/web/sondurumlar?merkezid={area}",
+            {"Origin": "https://mgm.gov.tr"},
+            context,
+        )
+    except:
+        return "GET failed"
+
+    try:
+        data = json.loads(data)[0]
+        code = data["hadiseKodu"]
+        temp = data["sicaklik"]
+        wind = data["ruzgarHiz"]
+        rain = data["yagis00Now"]
+
+        temp = f"{temp}°" if str(temp) != "-9999" else "n/a"
+        code = code.lower() if str(code) != "-9999" else "n/a"
+        wind = f"{wind:.1f}km/h" if str(wind) != "-9999" else "n/a"
+        rain = f"{rain}mm" if str(rain) != "-9999" else "n/a"
+
+        return f"{temp} {code} {wind} {rain}"
+    except:
+        return "parse failed"
 
 
-def get_mem():
+def module_yahoo():
+    try:
+        data = get_url("https://query1.finance.yahoo.com/v8/finance/chart/USDTRY=X")
+    except:
+        return "GET failed"
+
+    try:
+        return json.loads(data)["chart"]["result"][0]["meta"]["regularMarketPrice"]
+    except:
+        return "parse failed"
+
+
+def module_binance():
+    try:
+        data = get_url("https://api.binance.com/api/v3/ticker/price?symbol=USDTTRY")
+    except:
+        return "GET failed"
+
+    try:
+        return float(json.loads(data)["price"].rstrip("0"))
+    except:
+        return "parse failed"
+
+
+# - - tmpfs modules - - #
+def module_mem():
     with open("/proc/meminfo", "r", encoding="utf-8") as f:
         for line in f.readlines():
             if "MemAvailable" in line:
@@ -239,7 +264,7 @@ def get_mem():
     return mem_info
 
 
-def get_temp():
+def module_temp():
     hwmon_dir = "/sys/class/hwmon/"
     labels = ["Tctl", "Package", "Core 0"]
 
@@ -257,14 +282,15 @@ def get_temp():
             tempfile = re.sub(r"label", "input", file)
             break
 
-    if tempfile is None:
-        return None
+    if tempfile:
+        with open(tempfile, "r", encoding="utf-8") as f:
+            return f"{int(int(f.read()) / 1000)}c"
 
-    with open(tempfile, "r", encoding="utf-8") as f:
-        return f"{int(int(f.read()) / 1000)}c"
+    return "no tempfile"
 
 
-def get_ibmfan():
+def module_ibmfan():
+    rpm, level = "", ""
     with open(IBMFAN_PATH, "r", encoding="utf-8") as f:
         for line in f.readlines():
             if "speed:" in line:
@@ -281,7 +307,7 @@ def get_ibmfan():
     return f"{rpm}/{level}"
 
 
-def get_battery():
+def module_battery():
     bat0_path = "/sys/class/power_supply/BAT0"
 
     if not os.path.exists(f"{bat0_path}/status"):
@@ -306,7 +332,7 @@ def get_battery():
     return f"{bat_stat}{bat_cap}"
 
 
-def get_bandwidth():
+def module_bandwidth():
     iface = None
     with open("/proc/net/route", "r", encoding="utf-8") as f:
         for line in f.readlines():
@@ -348,86 +374,87 @@ def get_bandwidth():
     return f"{iface} {rx_rate}↓↑{tx_rate} [{rx_total}+{tx_total}]"
 
 
-def get_date():
+# - - misc modules - - #
+def module_date():
     return time.strftime("%a %d %H:%M:%S")
 
 
+def module_loadavg():
+    return f"{os.getloadavg()[0]:.2f}"
+
+
+# - - handlers - - #
+IBMFAN_PATH = "/proc/acpi/ibm/fan"
+MODULE_LIST = {
+    "mpd": module_mpd,
+    "mgm": module_mgm,
+    "yahoo": module_yahoo,
+    "binance": module_binance,
+    "mem": module_mem,
+    "temp": module_temp,
+    "ibmfan": module_ibmfan,
+    "battery": module_battery,
+    "bandwidth": module_bandwidth,
+    "date": module_date,
+    "loadavg": module_loadavg,
+}
+
+
+def handle_tmux():
+    p_set = "#[fg=white bg=black]"
+    p_reset = "#[default]"
+    p_block = f"{p_reset} {p_set}"
+
+    finprint = f"{p_set} "
+    if platform.system() == "Linux":
+        finprint += f"{module_mpd()} {p_block} "
+        finprint += f"{module_date()} {p_block} "
+        finprint += f"{module_loadavg()} {module_mem()} {p_block} "
+        finprint += f"{module_battery()}"
+    else:
+        finprint += f"{module_date()} {p_block} {module_loadavg()}"
+
+    return finprint
+
+
+def handle_modules(args):
+    try:
+        if args[0] in MODULE_LIST:
+            if args[0] == "mgm":
+                try:
+                    return print(MODULE_LIST["mgm"](args[1]))
+                except IndexError:
+                    print("mgm module requires an area code")
+                    sys.exit(1)
+            return print(MODULE_LIST[args[0]]())
+    except IndexError:
+        print("must specify a module name")
+        sys.exit(1)
+
+    print(f"module {args[0]} does not exist")
+    sys.exit(1)
+
+
+# - - action - - #
+def help_prompt():
+    print("statusbar.py {module <module> <module_args>|tmux}")
+    sys.exit(1)
+
+
+def run():
+    args = sys.argv[1:]
+
+    if not args or args[0] not in ["module", "tmux"]:
+        help_prompt()
+
+    if args[0] == "tmux":
+        return print(handle_tmux())
+
+    if args[0] == "module":
+        return handle_modules(args[1:])
+
+    help_prompt()
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Statusbar generator for tmux and X11")
-    subparsers = parser.add_subparsers(dest="command", required=True)
-
-    tmux = subparsers.add_parser(
-        "tmux", help="Print in the tmux specific format, without a loop"
-    )
-    tmux.add_argument("--host", type=str, default="localhost", help="MPD host")
-    tmux.add_argument("--port", type=int, default=6600, help="MPD port")
-
-    x11 = subparsers.add_parser("x11", help="Call xsetroot, run in a loop")
-    x11.add_argument("--area", type=int, required=True, help="MGM area code")
-    x11.add_argument(
-        "--interval", default=60, help="Interval that the APIs should be queried at"
-    )
-    x11.add_argument("--host", type=str, default="localhost", help="MPD host")
-    x11.add_argument("--port", type=int, default=6600, help="MPD port")
-    x11.add_argument(
-        "--debug",
-        action="store_true",
-        help="Print to the stdout instead of calling xsetroot",
-    )
-
-    args = parser.parse_args()
-
-    if args.command == "x11":
-        interval = args.interval
-        host = args.host
-        port = args.port
-
-        err_sleep = 10
-        scrape = Scrape(args.area)
-
-        threads = []
-        mgm_thread = Thread(target=scrape.get_mgm, daemon=True)
-        threads.append(mgm_thread)
-
-        yahoo_thread = Thread(target=scrape.get_yahoo, daemon=True)
-        threads.append(yahoo_thread)
-
-        binance_thread = Thread(target=scrape.get_binance, daemon=True)
-        threads.append(binance_thread)
-
-        for job in threads:
-            job.start()
-
-        while True:
-            finprint = '"'
-            finprint += f" {get_bandwidth()}"
-            finprint += f" ¦ {get_loadavg()} {get_mem()} {get_temp()}"
-
-            if os.path.isfile(IBMFAN_PATH):
-                finprint += f"/{get_battery()} {get_ibmfan()}"
-
-            finprint += f" ¦ {scrape.mgm} ¦ {scrape.yahoo}/{scrape.binance}"
-            #           finprint += f" ¦ {MPD(args.host, args.port).run()}"
-            finprint += f" ¦ {get_date()}"
-            finprint += '"'
-
-            if args.debug is True:
-                sys.stdout.write("\x1b[1A\x1b[2K")
-                print(finprint)
-            else:
-                os.popen(f"xsetroot -name {finprint}")
-
-    if args.command == "tmux":
-        p_set, p_reset = "#[fg=white bg=black]", "#[default]"
-        p_block = f"{p_reset} {p_set}"
-
-        finprint = f"{p_set} "
-        if platform.system() == "Linux":
-            finprint += f"{MPD(args.host, args.port).run()} {p_block} "
-            finprint += f"{get_date()} {p_block} "
-            finprint += f"{get_loadavg()} {get_mem()} {p_block} "
-            finprint += f"{get_battery()}"
-        else:
-            finprint += f"{get_date()} {p_block} {get_loadavg()}"
-
-        print(f"{finprint} {p_reset} ")
+    run()
